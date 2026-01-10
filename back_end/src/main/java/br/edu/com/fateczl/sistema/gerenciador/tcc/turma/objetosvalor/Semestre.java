b8e4aeca443d591d74187236b4fa29ba3b41d0b1
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-01-10 15:31:48 -0300

    func(turma): implementar entidade Turma com validação de regras acadêmicas
    - Implementa entidade Turma com proteção contra turnos e disciplinas não suportados pelo curso.
    - Adiciona verificação de regra de negócio: assegura que apenas professores habilitados ministrem TG.
    - Integra os registros PeriodoLetivo, Ano e Semestre para gestão temporal.
    - Estabelece métodos fábrica e getters modernos.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/turma/objetosvalor/Semestre.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/turma/objetosvalor/Semestre.java
@@ -1,4 +1,17 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.turma.objetosvalor;
 
-public record Semestre() {
+import br.edu.com.fateczl.sistema.gerenciador.tcc.compartilhado.excecoes.CodigoErro;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.compartilhado.excecoes.ValidacaoExcecao;
+
+public record Semestre(Integer valor) {
+    public Semestre {
+        if(valor == null) {
+            throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "semestre");
+        }
+        if(valor < 1 || valor > 2) {
+            throw new ValidacaoExcecao(CodigoErro.VD_005_PADRAO_INVALIDO,
+                    "semestre", "1 ou 2");
+        }
+    }
 }
--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/turma/objetosvalor/TurmaId.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/turma/objetosvalor/TurmaId.java
@@ -1,4 +1,17 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.turma.objetosvalor;
 
-public record TurmaId() {
+import br.edu.com.fateczl.sistema.gerenciador.tcc.compartilhado.excecoes.CodigoErro;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.compartilhado.excecoes.ValidacaoExcecao;
+
+public record TurmaId(Long valor) {
+    public TurmaId {
+        if(valor == null) {
+            throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "ID da turma");
+        }
+        if(valor <= 0) {
+            throw new ValidacaoExcecao(CodigoErro.VD_002_FORMATO_INVALIDO,
+                    "ID da turma", "valor positivo");
+        }
+    }
 }
