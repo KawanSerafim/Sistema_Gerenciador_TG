02d7a969942b03d9a6441382942de11fdfff19a3
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-12 20:35:52 -0300

    feat(shared): definir interface Coorientador para papéis polimórficos
    - Cria interface Coorientador para unificar a atuação de professores e externos no TCC.
    - Define contrato básico para obtenção de nome e identificação acadêmica/profissional.
    - Facilita o desacoplamento na entidade GrupoTcc.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/compartilhado/identificadores/Coorientador.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/compartilhado/identificadores/Coorientador.java
@@ -1,4 +1,8 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.compartilhado.identificadores;
 
+import br.edu.com.fateczl.sistema.gerenciador.tcc.compartilhado.objetosvalor.Nome;
+
 public interface Coorientador {
+    Nome nome();
+    String identificacao();
 }
