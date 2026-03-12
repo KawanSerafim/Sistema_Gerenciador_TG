59c70f95da8b30a7ab9110d42e44ceb45b3b177e
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-12 20:34:08 -0300

    feat(grupotcc): criar record GrupoTccId para identificação do grupo
    - Implementa record para identidade tipada do Grupo de TCC.
    - Garante que a identidade seja protegida contra valores inválidos.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/grupotcc/objetosvalor/GrupoTccId.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/grupotcc/objetosvalor/GrupoTccId.java
@@ -1,4 +1,17 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.grupotcc.objetosvalor;
 
-public record GrupoTccId() {
+import br.edu.com.fateczl.sistema.gerenciador.tcc.compartilhado.excecoes.CodigoErro;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.compartilhado.excecoes.ValidacaoExcecao;
+
+public record GrupoTccId(Long valor) {
+    public GrupoTccId {
+        if(valor == null) {
+            throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "ID do grupo de TCC");
+        }
+        if(valor <= 0) {
+            throw new ValidacaoExcecao(CodigoErro.VD_002_FORMATO_INVALIDO,
+                    "ID do grupo de TCC", "valor positivo");
+        }
+    }
 }
