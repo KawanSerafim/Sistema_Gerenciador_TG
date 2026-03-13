07546edca11c3d772df91e4aed7499940359aa86
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-13 20:18:25 -0300

    feat(repositorio): criar o repositório (porta de saída) de domínio do professor.
    - método para salvar o objeto Professor.
    
    - método para buscar o objeto Professor via matrícula.
    
    - método para buscar o objeto Professor via e-mail.

---
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/professor/dominio/repositorio/ProfessorRepositorio.java
@@ -0,0 +1,13 @@
+package br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.repositorio;
+
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.objetosvalor.Matricula;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.Email;
+import br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.entidade.Professor;
+
+import java.util.Optional;
+
+public interface ProfessorRepositorio {
+    void salvar(Professor professor);
+    Optional<Professor> buscarPorMatricula(Matricula matricula);
+    Optional<Professor> buscarPorEmail(Email email);
+}
