bdc16ab32efc580999abc11c9ed432b6b7cae87f
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-13 20:52:45 -0300

    feat(repositorio ): criar o repositório (porta de saída) de domínio do curso.
    - método para salvar e retornar o objeto Curso.
    
    - método para buscar o objeto Curso via nome.
    
    - método para buscar o objeto via coordenador.

---
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/repositorio/CursoRepositorio.java
@@ -0,0 +1,13 @@
+package br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.repositorio;
+
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.objetosvalor.Nome;
+import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.entidade.Curso;
+import br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.entidade.Professor;
+
+import java.util.Optional;
+
+public interface CursoRepositorio {
+    Curso salvar(Curso curso);
+    Optional<Curso> buscarPorNome(Nome nome);
+    Optional<Curso> buscarPorCoordenador(Professor professor);
+}
