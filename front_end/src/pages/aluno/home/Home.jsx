5c346afd5a89d6126ca7911b83a9e7e1e14d5f20
Thiago Antenor <thiagoantenor31@gmail.com>
2026-03-14 20:09:13 -0300

    feat: [home do aluno] add opção de sair na navbar

--- front_end/src/pages/aluno/home/Home.jsx
+++ front_end/src/pages/aluno/home/Home.jsx
@@ -3,56 +3,57 @@ import UserNavBar from "../../../components/usernavbar/UserNavBar"
 
 const Home = () => {
     const nomeAluno = "Max"
-    return(
-            <>
-                <UserNavBar
-                    userName={nomeAluno}
-                />
-                <Container className="mt-5 " style={{ minWidth: '800px' }}>
-                   <div className="border border-dark rounded-4 shadow-sm">
+    return (
+        <>
+            <UserNavBar
+                userName={nomeAluno}
+                opcoes={["sair"]}
+            />
+            <Container className="mt-5 " style={{ minWidth: '800px' }}>
+                <div className="border border-dark rounded-4 shadow-sm">
 
-                        <h2 className='bg-primary text-white p-3 fs-1 rounded-top-4 text-center mb-5'>Seja Bem vindo, {nomeAluno} </h2>
+                    <h2 className='bg-primary text-white p-3 fs-1 rounded-top-4 text-center mb-5'>Seja Bem vindo, {nomeAluno} </h2>
 
                     {/*Criar Grupo e Solicitar orientação */}
-                        <div className="d-flex justify-content-around gap-5 m-3">
-                           
-                            <Button variant="primary"
-                                    type="submit"
-                                    id='btn-criar-grupo' className='fs-4 fw-medium w-100'
-                                    >
-                                Criar Grupo 
-                            </Button> 
-                       
-                            <Button variant="primary"
-                                type="submit"
-                                id='btn-criar-grupo' className='fs-4 fw-medium w-100'
-                                >
-                                Solicitar orientação
-                            </Button> 
-                                
-                        </div>
+                    <div className="d-flex justify-content-around gap-5 m-3">
 
-                        {/*Sair do Grupo e Cadastrar Co-orientador */}
-                        <div className="d-flex justify-content-around gap-5 m-3">
-                           
-                                <Button variant="primary"
-                                    type="submit"
-                                    id='btn-sair-grupo' className='fs-4 fw-medium w-100'
-                                    >
-                                Sair do Grupo 
-                            </Button> 
-                           
-                                <Button variant="primary"
-                                    type="submit"
-                                    id='btn-cadastrar-co-orientador' className='fs-4 fw-medium w-100'
-                                    >
-                                Cadastrar Co-orientador
-                            </Button> 
-                                        
-                        </div>
+                        <Button variant="primary"
+                            type="submit"
+                            id='btn-criar-grupo' className='fs-4 fw-medium w-100'
+                        >
+                            Criar Grupo
+                        </Button>
+
+                        <Button variant="primary"
+                            type="submit"
+                            id='btn-criar-grupo' className='fs-4 fw-medium w-100'
+                        >
+                            Solicitar orientação
+                        </Button>
+
+                    </div>
+
+                    {/*Sair do Grupo e Cadastrar Co-orientador */}
+                    <div className="d-flex justify-content-around gap-5 m-3">
+
+                        <Button variant="primary"
+                            type="submit"
+                            id='btn-sair-grupo' className='fs-4 fw-medium w-100'
+                        >
+                            Sair do Grupo
+                        </Button>
+
+                        <Button variant="primary"
+                            type="submit"
+                            id='btn-cadastrar-co-orientador' className='fs-4 fw-medium w-100'
+                        >
+                            Cadastrar Co-orientador
+                        </Button>
+
+                    </div>
                 </div>
-                </Container>
-            </>
+            </Container>
+        </>
     )
 }
 export default Home
