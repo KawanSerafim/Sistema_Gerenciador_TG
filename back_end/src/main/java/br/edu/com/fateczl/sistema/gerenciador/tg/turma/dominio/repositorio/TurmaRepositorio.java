96f7c088c5dd5bd79c52ebac722521b3dc331843
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-13 21:08:06 -0300

    feat(repositorio ): criar o repositório (porta de saída) de domínio da turma.
    - método para salvar e retornar o objeto Turma.
    
    - método para buscar o objeto Turma via ID.
    
    - método para buscar o objeto Turma via curso, disciplina, turno, ano e semestre.

---
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/turma/dominio/repositorio/TurmaRepositorio.java
@@ -0,0 +1,18 @@
+package br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.repositorio;
+
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.objetosvalor.Disciplina;
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.objetosvalor.Turno;
+import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.entidade.Curso;
+import br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.entidade.Turma;
+import br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.objetosvalor.Ano;
+import br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.objetosvalor.Semestre;
+import br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.objetosvalor.TurmaId;
+
+import java.util.Optional;
+
+public interface TurmaRepositorio {
+    Turma salvar(Turma turma);
+    Optional<Turma> buscarPorId(TurmaId id);
+    Optional<Turma> buscarPorCursoEDisciplinaETurnoEAnoESemestre(Curso curso,
+            Disciplina disciplina, Turno turno, Ano ano, Semestre semestre);
+}
