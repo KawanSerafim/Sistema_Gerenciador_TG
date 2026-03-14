032a8cba7cbf16d5d6ebd0655c0241d86453a6cf
Thiago Antenor <thiagoantenor31@gmail.com>
2026-03-14 20:06:06 -0300

    refactor: refatorção da navBar para reuso em paginas com poucas opções

--- front_end/src/components/usernavbar/UserNavBar.jsx
+++ front_end/src/components/usernavbar/UserNavBar.jsx
@@ -1,28 +1,61 @@
-import { Container } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import './Styles.css';
 
-const UserNavBar = ({ userName = '' }) => {
-    const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+const UserNavBar = ({ userName = '', opcoes = ["inicio", "nome", "sair"], maxWidth = "900px" }) => {
+    const handleSelect = () => alert(`Redirecionando .... `);
+    let largura = "";
+    if (opcoes.length == 1) {
+        largura = "15%"
+    }
+    else if (opcoes.length == 2) {
+        largura = "75%"
+    }
+    else if (opcoes.length == 3) {
+        largura = "100%"
+    }
     return (
         <>
-            <Container className="mt-2">
-                <Nav variant="pills" activeKey="1" onSelect={handleSelect} className='bg-primary rounded d-flex justify-content-between'>
-                    <Nav.Item>
-                        <Nav.Link eventKey='1' href="#/home" className='text-white nav__link fs-3 fw-bold'>
-                            Inicio
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link disabled="disabled" className='text-white fs-3 fw-bold'>
-                            Olá, { userName }
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey='3' href="#/home" className='text-white nav__link fs-3 fw-bold'>
-                            Sair
-                        </Nav.Link>
-                    </Nav.Item>
+            <Container className="mt-2" style={{ maxWidth: maxWidth }}>
+                <Nav
+                    variant="pills"
+                    activeKey="1"
+                    onSelect={handleSelect}
+                    className='bg-primary rounded d-flex align-items-center px-3'
+                    style={{ minHeight: '60px', width: largura }}>
+
+                    <div className="d-flex flex-1">
+
+                        {opcoes.includes("inicio") &&
+                            (
+                                <Nav.Item>
+                                    <Nav.Link eventKey="1" href="#/home" className='text-white nav__link fs-3 fs-md-4 fw-bold'>
+                                        Inicio
+                                    </Nav.Link>
+                                </Nav.Item>
+                            )
+
+                        }
+                    </div>
+                    <div className="d-flex flex-grow-1 justify-content-center">
+                        {opcoes.includes("nome") && (
+                            <Nav.Item className="mx-auto">
+                                <Navbar.Text disabled="disabled" className='text-white fs-3 fs-md-4 fw-bold'>
+                                    Olá, {userName}
+                                </Navbar.Text>
+                            </Nav.Item>
+
+                        )}
+                    </div>
+                    <div className="d-flex flex-1 justify-content-end">
+                        {opcoes.includes("sair") && (
+                            <Nav.Item className={opcoes.includes("nome") ? "" : "ms-auto"}>
+                                <Nav.Link eventKey='3' href="#/home" className='text-white nav__link fs-3 fs-md-4 fw-bold'>
+                                    {userName != "" ? "Sair" : "Voltar"}
+                                </Nav.Link>
+                            </Nav.Item>
+                        )}
+                    </div>
                 </Nav>
             </Container>
         </>
