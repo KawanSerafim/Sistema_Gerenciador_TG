be157241e79badb70b7313adc9f7a5cff87027cb
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-15 19:02:45 -0300

    refactor(RemetenteEmail): Foi corrigido o nome da interface.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/contausuario/aplicacao/portas/RemetenteEmail.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/contausuario/aplicacao/portas/RemetenteEmail.java
@@ -2,6 +2,6 @@ package br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.aplicacao.portas;
 
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.Email;
 
-public interface RementeEmail {
+public interface RemetenteEmail {
     void enviarEmailTexto(Email destinatario, String assunto, String mensagem);
 }
