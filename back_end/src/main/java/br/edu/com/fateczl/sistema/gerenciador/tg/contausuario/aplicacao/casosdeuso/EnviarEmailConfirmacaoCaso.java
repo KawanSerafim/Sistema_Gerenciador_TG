153dd2e48b506dd98520b9c799b32fabc0eaaed4
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-15 19:00:56 -0300

    refactor(contausuario): modularizar logica e aplicar CodigoErro no EnviarEmailConfirmacaoCaso
    Divide a classe em metodos auxiliares de responsabilidade unica (procurarConta, validarStatusConta, gerarCodigo, publicarEmail) e substitui o lancamento de excecoes estaticas pelas novas GenericaExcecao e RegraNegocioExcecao integradas ao CodigoErro.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/contausuario/aplicacao/casosdeuso/EnviarEmailConfirmacaoCaso.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/contausuario/aplicacao/casosdeuso/EnviarEmailConfirmacaoCaso.java
@@ -1,4 +1,76 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.aplicacao.casosdeuso;
 
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.excecoes.CodigoErro;
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.excecoes.GenericaExcecao;
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.excecoes.RegraNegocioExcecao;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.aplicacao.portas.GerenciadorCacheCodigo;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.aplicacao.portas.RemetenteEmail;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.entidade.ContaUsuario;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.Email;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.StatusContaUsuario;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.repositorio.ContaUsuarioRepositorio;
+
+import java.util.Random;
+
 public class EnviarEmailConfirmacaoCaso {
+    private final ContaUsuarioRepositorio repositorio;
+    private final GerenciadorCacheCodigo cache;
+    private final RemetenteEmail remetente;
+
+    public EnviarEmailConfirmacaoCaso(ContaUsuarioRepositorio repositorio,
+                                      GerenciadorCacheCodigo cache,
+                                      RemetenteEmail remetente) {
+        this.repositorio = repositorio;
+        this.cache = cache;
+        this.remetente = remetente;
+    }
+
+    public record Comando(String email) {}
+
+    public void executar(Comando comando) {
+        Email emailAlvo = new Email(comando.email());
+        ContaUsuario conta = procurarConta(emailAlvo);
+        validarStatusConta(conta);
+
+        String codigo = gerarCodigoAlfanumerico();
+        cache.salvarCodigo(emailAlvo, codigo);
+
+        publicarEmail(codigo, emailAlvo);
+    }
+
+    private ContaUsuario procurarConta(Email emailAlvo) {
+        return repositorio.buscarPorEmail(emailAlvo).orElseThrow(() ->
+                new GenericaExcecao(CodigoErro.GN_001_REGISTRO_NAO_ENCONTRADO));
+    }
+
+    private void validarStatusConta(ContaUsuario conta) {
+        if(conta.status() == StatusContaUsuario.ATIVO) {
+            throw new RegraNegocioExcecao(
+                    CodigoErro.RN_001_ESTADO_INVALIDO_PARA_ACAO, "conta de " +
+                    "usuário", "VERIFICACAO_CODIGO_PENDENTE");
+        }
+    }
+
+    private String gerarCodigoAlfanumerico() {
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv" +
+                "wxyz0123456789";
+        StringBuilder codigo = new StringBuilder(6);
+        Random random = new Random();
+
+        for(int i = 0; i < 6; i++) {
+            codigo.append(caracteres.charAt(random.nextInt(
+                    caracteres.length())));
+        }
+        return codigo.toString();
+    }
+
+    private void publicarEmail(String codigo, Email emailAlvo) {
+        String assunto = "Confirme seu Email - SISTEMA DE TG";
+        String mensagem = "Olá e seja-vindo!\n\n"
+                + "Você está a um passo de concluir seu cadastro. Insira o "
+                + "código abaixo:\n\n"
+                + "Código: " + codigo + "\n\n"
+                + "Atenção: Este código expira em 15 minutos.";
+        remetente.enviarEmailTexto(emailAlvo, assunto, mensagem);
+    }
 }
