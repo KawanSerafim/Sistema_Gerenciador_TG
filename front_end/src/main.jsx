da4958f73cf69962304d96f8332a4c9a220f8590
Thiago Antenor <thiagoantenor31@gmail.com>
2026-03-17 20:48:27 -0300

    refactor: organizado rotas das paginas

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -17,36 +17,37 @@ import CadastrarTurma from './pages/professor/coordenador/cadastrarTurma/Cadastr
 import CarregarTG from './pages/aluno/carregarTG/CarregarTG.jsx';
 import GerarCertificado from './pages/professor/gerarCertificado/GerarCertificado.jsx';
 import MarcarBanca from './pages/professor/marcarBanca/MarcarBanca.jsx';
-import VisaoBancasArtigos from './pages/professor/visaoBancasArtigos/visaoBancasArtigos.jsx';
+
 import FinalizarDisciplinas from './pages/professor/finalizarDisciplinas/FinalizarDisciplinas.jsx';
 import Login from './pages/utils/login/Login.jsx';
 import SolicitarOrientacao from './pages/aluno/solicitarOrientacao/SolicitarOrientacao.jsx';
 import Home from './pages/aluno/home/Home.jsx';
 import CadastrarProfessor from './pages/adm/CadastrarProfessor.jsx';
+import VisaoBancasArtigos from './pages/professor/visaoBancasArtigos/VisaoBancasArtigos.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/adm/cadastarProfessor" element={<CadastrarProfessor />} />
-        <Route path="/aluno/cadastro" element={<CadastroAluno />} />
-        <Route path="/aluno/formarGrupo" element={<FormarGrupo />} />
-        <Route path="/aluno/solicitarOrientacao" element={<SolicitarOrientacao />} />
         <Route path="/professor/cadastro" element={<CadastroProfessor />} />
         <Route path="/confirmarEmail" element={<ConfirmarEmail />} />
         <Route path="/curso/cadastro" element={<CadastrarCurso />} />
-        <Route path="/professor/visaoSolicitarOrientacao" element={<VisaoSolicitacaoOrientacao />} />
-        <Route path="/professor/enviarTurma" element={<EnviarTurma />} />
-        <Route path="/professor/visaoGrupos" element={<VisaoGrupos />} />
-        <Route path="/professor/visaoAlunosEnviados" element={<VisaoAlunosEnviados />} />
         <Route path="/coordenador/cadastrarTurmaTG" element={<CadastrarTurma />} />
-        <Route path="/aluno/enviarTG" element={<CarregarTG />} />
+        <Route path="/professor/enviarTurma" element={<EnviarTurma />} />
+        <Route path="/professor/visaoAlunosEnviados" element={<VisaoAlunosEnviados />} />
+        <Route path="/professor/visaoSolicitacoesOrientacao" element={<VisaoSolicitacaoOrientacao />} />
+        <Route path="/professor/visaoGrupos" element={<VisaoGrupos />} />
         <Route path="/professor/certificados" element={<GerarCertificado />} />
         <Route path="/professor/marcarBanca" element={<MarcarBanca />} />
         <Route path="/professor/visaoBancas" element={<VisaoBancasArtigos />} />
         <Route path="/professor/finalizarDisciplinas" element={<FinalizarDisciplinas />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/aluno/cadastro" element={<CadastroAluno />} />
         <Route path="/aluno/home" element={<Home />} />
+        <Route path="/aluno/formarGrupo" element={<FormarGrupo />} />
+        <Route path="/aluno/solicitarOrientacao" element={<SolicitarOrientacao />} />
+        <Route path="/aluno/enviarTG" element={<CarregarTG />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
