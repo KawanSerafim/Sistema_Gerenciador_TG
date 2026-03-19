acf62c2daf55c830c27398ead824d9ecd259c816
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-18 21:36:32 -0300

    refactor(CursoId): Trocado o tipo do ID de Long para UUID.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/objetosvalor/CursoId.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/objetosvalor/CursoId.java
@@ -3,15 +3,13 @@ package br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.objetosvalor;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.excecoes.CodigoErro;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.excecoes.ValidacaoExcecao;
 
-public record CursoId(Long valor) {
+import java.util.UUID;
+
+public record CursoId(UUID valor) {
     public CursoId {
         if(valor == null) {
             throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
                     "ID do curso");
-        }
-        if(valor <= 0) {
-            throw new ValidacaoExcecao(CodigoErro.VD_002_FORMATO_INVALIDO,
-                    "ID do curso", "valor positivo");
         }
     }
 }
