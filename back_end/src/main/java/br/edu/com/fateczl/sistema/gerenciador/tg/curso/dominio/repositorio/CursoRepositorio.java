312b8a882c60f765b1f2bbdd323407cd81d0b015
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-21 17:20:05 -0300

    feat(curso): adicionado um método para buscar cursos pelo ID.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/repositorio/CursoRepositorio.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/repositorio/CursoRepositorio.java
@@ -2,12 +2,14 @@ package br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.repositorio;
 
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.objetosvalor.Nome;
 import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.entidade.Curso;
+import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.objetosvalor.CursoId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.entidade.Professor;
 
 import java.util.Optional;
 
 public interface CursoRepositorio {
     Curso salvar(Curso curso);
+    Optional<Curso> buscarPorId(CursoId id);
     Optional<Curso> buscarPorNome(Nome nome);
     Optional<Curso> buscarPorCoordenador(Professor professor);
 }
