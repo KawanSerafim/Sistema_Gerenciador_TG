dbeb2d29dcca7042eb22b8a63ac02a82b2a0f3a3
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-21 16:19:19 -0300

    refactor(grupo de tg): trocado o tipo do ID de Long para UUID.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/grupotg/dominio/objetosvalor/GrupoTgId.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/grupotg/dominio/objetosvalor/GrupoTgId.java
@@ -3,15 +3,13 @@ package br.edu.com.fateczl.sistema.gerenciador.tg.grupotg.dominio.objetosvalor;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.CodigoErro;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.ValidacaoExcecao;
 
-public record GrupoTgId(Long valor) {
+import java.util.UUID;
+
+public record GrupoTgId(UUID valor) {
     public GrupoTgId {
         if(valor == null) {
             throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
                     "ID do grupo de TG");
-        }
-        if(valor <= 0) {
-            throw new ValidacaoExcecao(CodigoErro.VD_002_FORMATO_INVALIDO,
-                    "ID do grupo de TG", "valor positivo");
         }
     }
 }
