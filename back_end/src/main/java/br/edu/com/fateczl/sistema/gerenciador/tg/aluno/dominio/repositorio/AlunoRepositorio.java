4b294d3065f87b41dd368c96a5dcf3b6e9678be9
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-21 17:19:30 -0300

    feat(aluno): adicionado um método para buscar alunos por mais de uma matricula.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/aluno/dominio/repositorio/AlunoRepositorio.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/aluno/dominio/repositorio/AlunoRepositorio.java
@@ -4,10 +4,12 @@ import br.edu.com.fateczl.sistema.gerenciador.tg.aluno.dominio.entidade.Aluno;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.objetosvalor.Matricula;
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.Email;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AlunoRepositorio {
     Aluno salvar(Aluno aluno);
     Optional<Aluno> buscarPorMatricula(Matricula matricula);
+    Optional<List<Aluno>> buscarPorMatriculas(List<Matricula> matriculas);
     Optional<Aluno> buscarPorEmail(Email email);
 }
