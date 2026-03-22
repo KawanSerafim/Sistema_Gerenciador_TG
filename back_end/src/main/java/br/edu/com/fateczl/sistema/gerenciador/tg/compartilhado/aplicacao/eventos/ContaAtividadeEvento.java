5b7ec6a3f34cb2c1db219226812c3eb6c17aa1bf
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-22 18:56:55 -0300

    feat(compartilhado): adicionado um novo evento para representar um conta que aguarda ser ativada pelo sistema.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/compartilhado/aplicacao/eventos/ContaAtividadeEvento.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/compartilhado/aplicacao/eventos/ContaAtividadeEvento.java
@@ -1,4 +1,5 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.aplicacao.eventos;
 
-public record ContaAtividadeEvento() {
-}
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.ContaUsuarioId;
+
+public record ContaAtividadeEvento(ContaUsuarioId contaUsuarioId) {}
