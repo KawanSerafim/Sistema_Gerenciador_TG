2b12541784b597ac5d5e4458602e918f1643026e
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-22 15:44:16 -0300

    refactor(coorientador externo): trocado o tipo do ID de Long para UUID.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/coorientador/externo/dominio/objetosvalor/CoorientadorExternoId.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/coorientador/externo/dominio/objetosvalor/CoorientadorExternoId.java
@@ -3,15 +3,13 @@ package br.edu.com.fateczl.sistema.gerenciador.tg.coorientador.externo.dominio.o
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.CodigoErro;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.ValidacaoExcecao;
 
-public record CoorientadorExternoId(Long valor) {
+import java.util.UUID;
+
+public record CoorientadorExternoId(UUID valor) {
     public CoorientadorExternoId {
         if(valor == null) {
             throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
                     "ID do coorientador externo");
-        }
-        if(valor <= 0) {
-            throw new ValidacaoExcecao(CodigoErro.VD_002_FORMATO_INVALIDO,
-                    "ID do coorientador externo", "valor positivo");
         }
     }
 }
