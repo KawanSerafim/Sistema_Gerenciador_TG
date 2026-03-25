00b4e8ad2fb515a4fc5b55b64df6603842021e47
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-25 20:02:55 -0300

    refactor(turma): refatorado para que não tenha que referenciar as entidades de curso e professor, apenas seus respectivos IDs. Além de que a responsabilidade de validar o Professor de TG fica para o caso de uso responsável por gerar a entidade.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/turma/dominio/entidade/Turma.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/turma/dominio/entidade/Turma.java
@@ -1,49 +1,51 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.entidade;
 
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.CodigoErro;
-import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.RegraNegocioExcecao;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.ValidacaoExcecao;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.objetosvalor.Disciplina;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.objetosvalor.Turno;
-import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.entidade.Curso;
-import br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.entidade.Professor;
+import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.objetosvalor.CursoId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.objetosvalor.ProfessorId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.objetosvalor.PeriodoLetivo;
 import br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.objetosvalor.TurmaId;
 
 public class Turma {
     private final TurmaId id;
-    private final Curso curso;
+    private final CursoId cursoId;
     private final Disciplina disciplina;
     private final Turno turno;
     private final PeriodoLetivo periodoLetivo;
-    private Professor professorTg;
+    private ProfessorId professorTgId;
 
-    private Turma(TurmaId id, Curso curso, Disciplina disciplina, Turno turno,
-                  PeriodoLetivo periodoLetivo, Professor professorTg) {
+    private Turma(TurmaId id, CursoId cursoId, Disciplina disciplina,
+                  Turno turno,
+                  PeriodoLetivo periodoLetivo, ProfessorId professorTgId) {
         this.id = assegurarPresenca(id, "ID");
-        this.curso = assegurarPresenca(curso, "curso");
-        this.disciplina = validarDisciplina(disciplina, curso);
-        this.turno = validarTurno(turno, curso);
+        this.cursoId = assegurarPresenca(cursoId, "ID do curso");
+        this.disciplina = assegurarPresenca(disciplina, "disciplina");
+        this.turno = assegurarPresenca(turno, "turno");
         this.periodoLetivo = assegurarPresenca(periodoLetivo, "período letivo");
-        this.professorTg = validarProfessorTg(professorTg);
+        this.professorTgId = assegurarPresenca(professorTgId, "ID do " +
+                "professor de" +
+                " TG");
     }
 
     // Métodos Factory ---------------------------------------------------------
 
-    public static Turma novo(TurmaId id, Curso curso, Disciplina disciplina,
+    public static Turma novo(TurmaId id, CursoId cursoId, Disciplina disciplina,
                              Turno turno,
                              PeriodoLetivo periodoLetivo,
-                             Professor professorTg) {
-        return new Turma(id, curso, disciplina, turno, periodoLetivo,
-                professorTg);
+                             ProfessorId professorTgId) {
+        return new Turma(id, cursoId, disciplina, turno, periodoLetivo,
+                professorTgId);
     }
 
-    public static Turma carregar(TurmaId id, Curso curso, Disciplina disciplina,
+    public static Turma carregar(TurmaId id, CursoId cursoId,
+                                 Disciplina disciplina,
                                  Turno turno, PeriodoLetivo periodoLetivo,
-                                 Professor professorTg) {
-        return new Turma(id, curso, disciplina, turno, periodoLetivo,
-                professorTg);
+                                 ProfessorId professorTgId) {
+        return new Turma(id, cursoId, disciplina, turno, periodoLetivo,
+                professorTgId);
     }
 
     // Métodos especiais -------------------------------------------------------
@@ -56,48 +58,15 @@ public class Turma {
         return objeto;
     }
 
-    private Disciplina validarDisciplina(Disciplina disciplina, Curso curso) {
-        assegurarPresenca(disciplina, "disciplina");
-
-        if(!curso.validarDisciplina(disciplina)) {
-            throw new ValidacaoExcecao(CodigoErro.VD_007_CAMPO_NAO_SUPORTADO,
-                    "disciplina", "o curso associado não a possui");
-        }
-        return disciplina;
-    }
-
-    private Turno validarTurno(Turno turno, Curso curso) {
-        assegurarPresenca(turno, "turno");
-
-        if(!curso.validarTurno(turno)) {
-            throw new ValidacaoExcecao(CodigoErro.VD_007_CAMPO_NAO_SUPORTADO,
-                    "turno", "o curso associado não o possui");
-        }
-        return turno;
-    }
-
-    private Professor validarProfessorTg(Professor professorTg) {
-        assegurarPresenca(professorTg, "professor");
-
-        if(!professorTg.podeSerProfessorTg()) {
-            throw new RegraNegocioExcecao(
-                    CodigoErro.RN_001_ESTADO_INVALIDO_PARA_ACAO, "professor",
-                    "professor de TG");
-        }
-        return professorTg;
-    }
-
     // Métodos de Atualização --------------------------------------------------
 
-    public void atualizarProfessorTg(Professor novoProfessor) {
-        this.professorTg = assegurarPresenca(novoProfessor, "professor de TG");
+    public void atualizarProfessorTg(ProfessorId novoProfessorId) {
+        this.professorTgId = assegurarPresenca(novoProfessorId, "ID do " +
+                "professor de TG");
     }
 
     // Métodos Getters de Delegação --------------------------------------------
 
-    public String nomeCursoTexto() { return curso.nomeTexto(); }
-    public ProfessorId idProfessorTg() { return professorTg.id(); }
-    public ProfessorId idCoordenadorCurso() { return curso.idCoordenador(); }
     public Integer anoLetivoValor() { return periodoLetivo.anoValor(); }
     public Integer semestreLetivoValor() {
         return periodoLetivo.semestreValor();
@@ -107,9 +76,9 @@ public class Turma {
 
     public TurmaId id() { return id; }
     public String idTexto() { return id.valor().toString(); }
-    public Curso curso() { return curso; }
+    public CursoId cursoId() { return cursoId; }
     public Disciplina disciplina() { return disciplina; }
     public Turno turno() { return turno; }
     public PeriodoLetivo periodoLetivo() { return periodoLetivo; }
-    public Professor professorTg() { return professorTg; }
+    public ProfessorId professorTgId() { return professorTgId; }
 }
