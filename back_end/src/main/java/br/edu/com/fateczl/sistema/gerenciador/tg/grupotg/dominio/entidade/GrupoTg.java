2211be1b20af9bd0ca188be2e8453178752d7630
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-27 20:29:23 -0300

    refactor(grupo de TG): refatorado para que o grupo de TG não referencie o objeto inteiro dos alunos, orientador e coorientador, e sim seus IDs. Além disso, as disciplinas agora estão em Set, para engoblar os cenários em que alunos estão em TG1 e TG2.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/grupotg/dominio/entidade/GrupoTg.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/grupotg/dominio/entidade/GrupoTg.java
@@ -1,60 +1,57 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.grupotg.dominio.entidade;
 
-import br.edu.com.fateczl.sistema.gerenciador.tg.aluno.dominio.entidade.Aluno;
+import br.edu.com.fateczl.sistema.gerenciador.tg.aluno.dominio.objetosvalor.AlunoId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.CodigoErro;
-import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.RegraNegocioExcecao;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.ValidacaoExcecao;
-import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.identificadores.Coorientador;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.objetosvalor.Disciplina;
+import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.objetosvalor.CursoId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.objetosvalor.TipoTg;
-import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.entidade.Curso;
 import br.edu.com.fateczl.sistema.gerenciador.tg.grupotg.dominio.objetosvalor.GrupoTgId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.grupotg.dominio.objetosvalor.TemaTg;
-import br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.entidade.Professor;
-import br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.entidade.Turma;
+import br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.objetosvalor.ProfessorId;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class GrupoTg {
     private final GrupoTgId id;
-    private Professor orientador;
-    private Coorientador coorientador;
-    private final Curso curso;
-    private Disciplina disciplina;
+    private ProfessorId orientadorId;
+    private String coorientadorIdTexto;
+    private final CursoId cursoId;
+    private Set<Disciplina> disciplinas;
     private TemaTg temaTg;
     private TipoTg tipoTg;
-    private List<Aluno> alunos;
+    private List<AlunoId> alunosIds;
 
-    private GrupoTg(GrupoTgId id, Professor orientador,
-                    Coorientador coorientador, Curso curso,
-                    Disciplina disciplina, TemaTg temaTg, TipoTg tipoTg,
-                    List<Aluno> alunos) {
+    private GrupoTg(GrupoTgId id, ProfessorId orientadorId,
+                    String coorientadorIdTexto, CursoId cursoId,
+                    Set<Disciplina> disciplinas, TemaTg temaTg, TipoTg tipoTg,
+                    List<AlunoId> alunosIds) {
         this.id = assegurarPresenca(id, "ID");
-        this.orientador = orientador;
-        this.coorientador = coorientador;
-        this.curso = assegurarPresenca(curso, "curso");
-        this.disciplina = assegurarPresenca(disciplina, "disciplina");
+        this.orientadorId = orientadorId;
+        this.coorientadorIdTexto = coorientadorIdTexto;
+        this.cursoId = assegurarPresenca(cursoId, "ID do curso");
+        this.disciplinas = assegurarPresencaDisciplinas(disciplinas);
         this.temaTg = assegurarPresenca(temaTg, "tema de TG");
         this.tipoTg = assegurarPresenca(tipoTg, "tipo de TG");
-        this.alunos = new ArrayList<>(validarAlunos(alunos));
+        this.alunosIds = new ArrayList<>((assegurarPresencaAlunos(alunosIds)));
     }
 
     // Métodos Factory ---------------------------------------------------------
 
-    public static GrupoTg novo(GrupoTgId id, Curso curso, Disciplina disciplina,
+    public static GrupoTg novo(GrupoTgId id, CursoId cursoId,
+                               Set<Disciplina> disciplinas,
                                TemaTg temaTg, TipoTg tipoTg,
-                               List<Aluno> alunos) {
-        return new GrupoTg(id, null, null, curso, disciplina, temaTg, tipoTg,
-                alunos);
+                               List<AlunoId> alunosIds) {
+        return new GrupoTg(id, null, null, cursoId, disciplinas, temaTg, tipoTg,
+                alunosIds);
     }
 
-    public static GrupoTg carregar(GrupoTgId id, Professor orientador,
-                                   Coorientador coorientador, Curso curso,
-                                   Disciplina disciplina, TemaTg temaTg,
-                                   TipoTg tipoTg, List<Aluno> alunos) {
-        return new GrupoTg(id, orientador, coorientador, curso, disciplina,
-                temaTg, tipoTg, alunos);
+    public static GrupoTg carregar(GrupoTgId id, ProfessorId orientadorId,
+                                   String coorientadorIdTexto, CursoId cursoId,
+                                   Set<Disciplina> disciplinas, TemaTg temaTg,
+                                   TipoTg tipoTg, List<AlunoId> alunosIds) {
+        return new GrupoTg(id, orientadorId, coorientadorIdTexto, cursoId,
+                disciplinas, temaTg, tipoTg, alunosIds);
     }
 
     // Métodos especiais -------------------------------------------------------
@@ -67,64 +64,36 @@ public class GrupoTg {
         return objeto;
     }
 
-    private List<Aluno> validarAlunos(List<Aluno> alunos) {
-        if(alunos == null || alunos.isEmpty()) {
+    private Set<Disciplina> assegurarPresencaDisciplinas(
+            Set<Disciplina> disciplinas) {
+        if(disciplinas == null || disciplinas.isEmpty()) {
+            throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "disciplinas");
+        }
+        return EnumSet.copyOf(disciplinas);
+    }
+
+    private List<AlunoId> assegurarPresencaAlunos(List<AlunoId> alunosIds) {
+        if(alunosIds == null || alunosIds.isEmpty()) {
             throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
                     "alunos");
         }
-
-        Set<Disciplina> disciplinasMolde = alunos.getFirst().turmas().stream()
-                .map(Turma::disciplina)
-                .collect(Collectors.toSet());
-
-        for(Aluno aluno : alunos) {
-            Set<Disciplina> disciplinasDesteAluno = aluno.turmas().stream()
-                    .map(Turma::disciplina)
-                    .collect(Collectors.toSet());
-
-            if(!disciplinasDesteAluno.equals(disciplinasMolde)) {
-                throw new RegraNegocioExcecao(
-                        CodigoErro.RN_003_CONDICAO_ACAO_NAO_ATENDIDA,
-                        "alunos do grupo", "estar matriculado exatamente na " +
-                        "mesma composição de disciplinas");
-            }
-        }
-
-        if(!curso.validarQtdAlunosGrupo(tipoTg, alunos.size())) {
-            throw new RegraNegocioExcecao(CodigoErro
-                    .RN_004_LIMITE_ALUNOS_EXCEDIDO, alunos.size(),
-                    tipoTg.name(),
-                    curso.nomeTexto());
-        }
-        return alunos;
+        return alunosIds;
     }
 
-    public void vincularOrientador(Professor professor) {
-        assegurarPresenca(professor, "orientador");
-
-        if(!professor.podeSerOrientador()) {
-            throw new RegraNegocioExcecao(
-                    CodigoErro.RN_003_CONDICAO_ACAO_NAO_ATENDIDA, "professor",
-                    "para ser um orientador");
-        }
-        this.orientador = professor;
+    public void vincularOrientador(ProfessorId professorId) {
+        this.orientadorId = assegurarPresenca(professorId, "ID do orientador");
     }
 
-    public void vincularCoorientador(Coorientador coorientador) {
-        if(coorientador instanceof Professor professor
-                && !professor.podeSerOrientador()) {
-            throw new RegraNegocioExcecao(
-                    CodigoErro.RN_003_CONDICAO_ACAO_NAO_ATENDIDA,
-                    "coorientador",
-                    "para ser um orientador");
-        }
-        this.coorientador = coorientador;
+    public void vincularCoorientador(String coorientadorIdTexto) {
+        this.coorientadorIdTexto = assegurarPresenca(coorientadorIdTexto,
+                "ID do coorientador");
     }
 
     // Métodos de Atualização --------------------------------------------------
 
-    public void atualizarDisciplina(Disciplina novaDisciplina) {
-        this.disciplina = assegurarPresenca(novaDisciplina, "disciplina");
+    public void atualizarDisciplina(Set<Disciplina> novasDisciplinas) {
+        this.disciplinas = assegurarPresencaDisciplinas(novasDisciplinas);
     }
 
     public void atualizarTemaTg(TemaTg novoTemaTg) {
@@ -135,19 +104,24 @@ public class GrupoTg {
         this.tipoTg = assegurarPresenca(novoTipoTg, "tipo de TG");
     }
 
-    public void atualizarAlunos(List<Aluno> novosAlunos) {
-        this.alunos = new ArrayList<>(validarAlunos(novosAlunos));
+    public void atualizarAlunos(List<AlunoId> novosAlunosIds) {
+        this.alunosIds = new ArrayList<>(
+                assegurarPresencaAlunos(novosAlunosIds));
     }
 
     // Métodos Getters ---------------------------------------------------------
 
     public GrupoTgId id() { return id; }
     public String idTexto() { return id.valor().toString(); }
-    public Professor orientador() { return orientador; }
-    public Coorientador coorientador() { return coorientador; }
-    public Curso curso() { return curso; }
-    public Disciplina disciplina() { return disciplina; }
+    public ProfessorId orientadorId() { return orientadorId; }
+    public String coorientadorIdTexto() { return coorientadorIdTexto; }
+    public CursoId cursoId() { return cursoId; }
+    public Set<Disciplina> disciplinas() {
+        return Collections.unmodifiableSet(disciplinas);
+    }
     public TemaTg temaTg() { return temaTg; }
     public TipoTg tipoTg() { return tipoTg; }
-    public List<Aluno> alunos() { return Collections.unmodifiableList(alunos); }
+    public List<AlunoId> alunosIds() {
+        return Collections.unmodifiableList(alunosIds);
+    }
 }
