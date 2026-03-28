c7190e2d34462a029c357450c10ff34bc60ad0aa
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-28 15:23:09 -0300

    refactor(aluno): refatorado para utilizar os validadores de serviço e deixar o caso de uso mais limpo.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/aluno/aplicacao/casosdeuso/SolicitarAcessoAlunoCaso.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/aluno/aplicacao/casosdeuso/SolicitarAcessoAlunoCaso.java
@@ -1,13 +1,11 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.aluno.aplicacao.casosdeuso;
 
 import br.edu.com.fateczl.sistema.gerenciador.tg.aluno.dominio.entidade.Aluno;
-import br.edu.com.fateczl.sistema.gerenciador.tg.aluno.dominio.objetosvalor.StatusAluno;
 import br.edu.com.fateczl.sistema.gerenciador.tg.aluno.dominio.repositorio.AlunoRepositorio;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.aplicacao.eventos.ContaPendenteCriadaEvento;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.aplicacao.portas.PublicadorEventos;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.CodigoErro;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.GenericaExcecao;
-import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.RegraNegocioExcecao;
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.objetosvalor.Matricula;
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.aplicacao.portas.CriptografoSenhas;
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.entidade.ContaUsuario;
@@ -15,6 +13,7 @@ import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosval
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.Email;
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.Senha;
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.repositorio.ContaUsuarioRepositorio;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.servicos.VerificadorUnicidadeEmail;
 
 import java.util.UUID;
 
@@ -23,15 +22,20 @@ public class SolicitarAcessoAlunoCaso {
     private final ContaUsuarioRepositorio contaUsuarioRepositorio;
     private final CriptografoSenhas criptografo;
     private final PublicadorEventos publicador;
+    private final VerificadorUnicidadeEmail verificadorEmail;
 
-    public SolicitarAcessoAlunoCaso(AlunoRepositorio alunoRepositorio,
-                                    ContaUsuarioRepositorio contaUsuarioRepositorio,
-                                    CriptografoSenhas criptografo,
-                                    PublicadorEventos publicador) {
+    public SolicitarAcessoAlunoCaso(
+            AlunoRepositorio alunoRepositorio,
+            ContaUsuarioRepositorio contaUsuarioRepositorio,
+            CriptografoSenhas criptografo,
+            PublicadorEventos publicador,
+            VerificadorUnicidadeEmail verificadorEmail
+    ) {
         this.alunoRepositorio = alunoRepositorio;
         this.contaUsuarioRepositorio = contaUsuarioRepositorio;
         this.criptografo = criptografo;
         this.publicador = publicador;
+        this.verificadorEmail = verificadorEmail;
     }
 
     public record Comando(String matricula, String email, String senhaLimpa) {}
@@ -43,45 +47,40 @@ public class SolicitarAcessoAlunoCaso {
         Matricula matricula = new Matricula(comando.matricula());
         Email email = new Email(comando.email());
 
-        validarUnicidadeEmail(email);
-        Aluno aluno = buscarEValidarAluno(matricula);
+        verificadorEmail.verificar(email);
 
-        Senha senhaCriptografada = criptografo.criptografar(
-                comando.senhaLimpa());
+        Aluno aluno = buscarAluno(matricula);
+        aluno.validarSolicitacaoAcesso();
+
         ContaUsuarioId novaContaId = new ContaUsuarioId(UUID.randomUUID());
-        ContaUsuario novaConta = ContaUsuario.novo(novaContaId, email,
-                senhaCriptografada);
+        Senha senhaCriptografada = criptografo.criptografar(
+                comando.senhaLimpa()
+        );
+        ContaUsuario novaConta = ContaUsuario.novo(
+                novaContaId,
+                email,
+                senhaCriptografada
+        );
 
         aluno.vincularConta(novaContaId);
 
         contaUsuarioRepositorio.salvar(novaConta);
         alunoRepositorio.salvar(aluno);
 
-        publicador.publicar(new ContaPendenteCriadaEvento(
-                novaConta.emailTexto()));
+        publicador.publicar(
+                new ContaPendenteCriadaEvento(novaConta.emailTexto())
+        );
 
         return new Resposta(aluno.idTexto(), aluno.nomeTexto(), email.valor());
     }
 
-    // FLUXOS ESPECIALIZADOS ---------------------------------------------------
+    // FLUXO ESPECIALIZADO ---------------------------------------------------
 
-    private void validarUnicidadeEmail(Email email) {
-        contaUsuarioRepositorio.buscarPorEmail(email).ifPresent(conta -> {
-            throw new RegraNegocioExcecao(CodigoErro.RN_002_REGISTRO_DUPLICADO,
-                    "email");
-        });
-    }
-
-    private Aluno buscarEValidarAluno(Matricula matricula) {
-        Aluno aluno = alunoRepositorio.buscarPorMatricula(matricula)
+    private Aluno buscarAluno(Matricula matricula) {
+        return alunoRepositorio.buscarPorMatricula(matricula)
                 .orElseThrow(() -> new GenericaExcecao(
-                        CodigoErro.GN_001_REGISTRO_NAO_ENCONTRADO, "aluno"));
-
-        if(aluno.status() != StatusAluno.PRE_CADASTRO) {
-            throw new RegraNegocioExcecao(
-                    CodigoErro.RN_001_ESTADO_INVALIDO_PARA_ACAO, "aluno",
-                    "PRE_CADASTRO");
-        }
-        return aluno;
+                        CodigoErro.GN_001_REGISTRO_NAO_ENCONTRADO,
+                        "aluno"
+                ));
     }
 }
