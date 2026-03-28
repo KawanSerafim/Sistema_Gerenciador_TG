f408ab20439348a81474a94fa2d04cb818dda19e
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-28 14:39:11 -0300

    feat(curso): criado uma classe dedicada a verificar a unicidade do curso.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/servicos/VerificadorUnicidadeCurso.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/servicos/VerificadorUnicidadeCurso.java
@@ -1,4 +1,23 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.servicos;
 
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.CodigoErro;
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.RegraNegocioExcecao;
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.objetosvalor.Nome;
+import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.repositorio.CursoRepositorio;
+
 public class VerificadorUnicidadeCurso {
+    private final CursoRepositorio cursoRepositorio;
+
+    public VerificadorUnicidadeCurso(CursoRepositorio cursoRepositorio) {
+        this.cursoRepositorio = cursoRepositorio;
+    }
+
+    public void verificar(Nome nome) {
+        cursoRepositorio.buscarPorNome(nome).ifPresent(curso -> {
+            throw new RegraNegocioExcecao(
+                    CodigoErro.RN_002_REGISTRO_DUPLICADO,
+                    "nome do curso"
+            );
+        });
+    }
 }
