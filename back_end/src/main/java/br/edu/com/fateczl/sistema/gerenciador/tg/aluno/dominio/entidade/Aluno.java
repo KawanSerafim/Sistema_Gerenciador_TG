cc258915da29f64690d8dd386ebb7e7c63056a95
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-28 15:23:39 -0300

    feat(aluno): adicionado um método para validar a solicitação de acesso do aluno.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/aluno/dominio/entidade/Aluno.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/aluno/dominio/entidade/Aluno.java
@@ -20,52 +20,85 @@ public class Aluno {
     private StatusAluno status;
     private List<TurmaId> turmasIds;
 
-    private Aluno(AlunoId id, Nome nome, Matricula matricula,
-                  ContaUsuarioId contaUsuarioId, StatusAluno status,
-                  List<TurmaId> turmasIds) {
+    private Aluno(
+            AlunoId id,
+            Nome nome,
+            Matricula matricula,
+            ContaUsuarioId contaUsuarioId,
+            StatusAluno status,
+            List<TurmaId> turmasIds
+    ) {
         this.id = assegurarPresenca(id, "ID");
         this.nome = assegurarPresenca(nome, "nome");
         this.matricula = assegurarPresenca(matricula, "matrícula");
         this.contaUsuarioId = contaUsuarioId;
         this.status = assegurarPresenca(status, "status");
-        this.turmasIds = new ArrayList<>(assegurarPresenca(turmasIds,
-                "IDs das turmas"));
+        this.turmasIds = new ArrayList<>(
+                assegurarPresenca(turmasIds, "IDs das turmas")
+        );
     }
 
     // Métodos Factory ---------------------------------------------------------
 
-    public static Aluno novo(AlunoId id, Nome nome, Matricula matricula,
-                             TurmaId turmaInicial) {
+    public static Aluno novo(
+            AlunoId id,
+            Nome nome,
+            Matricula matricula,
+            TurmaId turmaInicial
+    ) {
         if(turmaInicial == null) {
-            throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
-                    "turma inicial");
+            throw new ValidacaoExcecao(
+                    CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "turma inicial"
+            );
         }
 
-        return new Aluno(id, nome, matricula, null, StatusAluno.PRE_CADASTRO,
-                List.of(turmaInicial));
+        return new Aluno(
+                id,
+                nome,
+                matricula,
+                null,
+                StatusAluno.PRE_CADASTRO,
+                List.of(turmaInicial)
+        );
     }
 
-    public static Aluno carregar(AlunoId id, Nome nome, Matricula matricula,
-                                 ContaUsuarioId contaUsuarioId,
-                                 StatusAluno status, List<TurmaId> turmasIds) {
-        return new Aluno(id, nome, matricula, contaUsuarioId, status,
-                turmasIds);
+    public static Aluno carregar(
+            AlunoId id,
+            Nome nome,
+            Matricula matricula,
+            ContaUsuarioId contaUsuarioId,
+            StatusAluno status,
+            List<TurmaId> turmasIds
+    ) {
+        return new Aluno(
+                id,
+                nome,
+                matricula,
+                contaUsuarioId,
+                status,
+                turmasIds
+        );
     }
 
     // Métodos especiais -------------------------------------------------------
 
     private <T> T assegurarPresenca(T objeto, String campo) {
         if(objeto == null) {
-            throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
-                    campo);
+            throw new ValidacaoExcecao(
+                    CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    campo
+            );
         }
         return objeto;
     }
 
     private List<TurmaId> assegurarPresencaTurmas(List<TurmaId> turmasIds) {
         if(turmasIds == null || turmasIds.isEmpty()) {
-            throw new ValidacaoExcecao(CodigoErro.VD_001_CAMPO_OBRIGATORIO,
-                    "IDs das turmas");
+            throw new ValidacaoExcecao(
+                    CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "IDs das turmas"
+            );
         }
         return turmasIds;
     }
@@ -82,11 +115,21 @@ public class Aluno {
         this.turmasIds.add(novaTurmaId);
     }
 
+    public void validarSolicitacaoAcesso() {
+        if(status != StatusAluno.PRE_CADASTRO) {
+            throw new RegraNegocioExcecao(
+                    CodigoErro.RN_001_ESTADO_INVALIDO_PARA_ACAO,
+                    "status do aluno", "PRE_CADASTRO"
+            );
+        }
+    }
+
     public void concluirCadastro() {
         if(status != StatusAluno.AGUARDANDO_CONFIRMACAO) {
             throw new RegraNegocioExcecao(
                     CodigoErro.RN_001_ESTADO_INVALIDO_PARA_ACAO,
-                    "status do aluno", "AGUARDANDO_CONFIRMACAO");
+                    "status do aluno", "AGUARDANDO_CONFIRMACAO"
+            );
         }
         this.status = StatusAluno.CADASTRADO;
     }
@@ -94,8 +137,10 @@ public class Aluno {
     // Métodos de Atualização --------------------------------------------------
 
     public void vincularConta(ContaUsuarioId novaContaUsuarioId) {
-        this.contaUsuarioId = assegurarPresenca(novaContaUsuarioId,
-                "ID da conta de usuário");
+        this.contaUsuarioId = assegurarPresenca(
+                novaContaUsuarioId,
+                "ID da conta de usuário"
+        );
 
         if(status == StatusAluno.PRE_CADASTRO) {
             this.status = StatusAluno.AGUARDANDO_CONFIRMACAO;
