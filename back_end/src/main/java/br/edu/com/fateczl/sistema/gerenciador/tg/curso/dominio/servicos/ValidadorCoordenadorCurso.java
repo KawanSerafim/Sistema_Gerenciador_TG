0f6e28df7368a0bd6eb5c40b27e13f51cf4098cc
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-28 14:40:03 -0300

    feat(curso): criado uma classe dedicada a validar os coordenadores do curso na hora da sua criação.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/servicos/ValidadorCoordenadorCurso.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/curso/dominio/servicos/ValidadorCoordenadorCurso.java
@@ -1,4 +1,16 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.servicos;
 
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.CodigoErro;
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.RegraNegocioExcecao;
+import br.edu.com.fateczl.sistema.gerenciador.tg.professor.dominio.entidade.Professor;
+
 public class ValidadorCoordenadorCurso {
+    public void validar(Professor professor) {
+        if(!professor.podeSerCoordenadorCurso()) {
+            throw new RegraNegocioExcecao(
+                    CodigoErro.RN_001_ESTADO_INVALIDO_PARA_ACAO,
+                    "professor", "coordenador de curso"
+            );
+        }
+    }
 }
