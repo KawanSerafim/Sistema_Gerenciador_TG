9ca3a9dfcfb2748b3eade38ee62550e28a105157
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-28 15:21:54 -0300

    feat(conta de usuário): criado uma classe dedicada a verificar a unicidade dos emails das conta de usuário na hora de sua criação.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/contausuario/dominio/servicos/VerificadorUnicidadeEmail.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/contausuario/dominio/servicos/VerificadorUnicidadeEmail.java
@@ -1,4 +1,23 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.servicos;
 
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.CodigoErro;
+import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.excecoes.RegraNegocioExcecao;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.Email;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.repositorio.ContaUsuarioRepositorio;
+
 public class VerificadorUnicidadeEmail {
+    private final ContaUsuarioRepositorio repositorio;
+
+    public VerificadorUnicidadeEmail(ContaUsuarioRepositorio repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public void verificar(Email email) {
+        repositorio.buscarPorEmail(email).ifPresent(conta -> {
+            throw new RegraNegocioExcecao(
+                    CodigoErro.RN_002_REGISTRO_DUPLICADO,
+                    "email"
+            );
+        });
+    }
 }
