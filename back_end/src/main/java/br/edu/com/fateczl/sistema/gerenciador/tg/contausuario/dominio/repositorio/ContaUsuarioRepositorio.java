708e0e695cf31b1c638e78f15503498574851543
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-03-29 19:23:20 -0300

    fix e feat(conta de usuário): arrumado para o método de salvar seja void e que tenha um método de busca por ID.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/contausuario/dominio/repositorio/ContaUsuarioRepositorio.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/contausuario/dominio/repositorio/ContaUsuarioRepositorio.java
@@ -1,11 +1,13 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.repositorio;
 
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.entidade.ContaUsuario;
+import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.ContaUsuarioId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.contausuario.dominio.objetosvalor.Email;
 
 import java.util.Optional;
 
 public interface ContaUsuarioRepositorio {
-    ContaUsuario salvar(ContaUsuario contaUsuario);
+    void salvar(ContaUsuario contaUsuario);
+    Optional<ContaUsuario> buscarPorId(ContaUsuarioId id);
     Optional<ContaUsuario> buscarPorEmail(Email email);
 }
