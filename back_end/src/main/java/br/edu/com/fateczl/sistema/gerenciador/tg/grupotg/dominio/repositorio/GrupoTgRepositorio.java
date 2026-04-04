8b2e3677d5e3b6b5453044d3d0b3385bf32080e1
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-04-03 22:03:29 -0300

    fix(grupo de TG): arrumado para o método de salvar não ter retorno. Também trocado o antigo método de busca para agora ser através de um aluno e um curso.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/grupotg/dominio/repositorio/GrupoTgRepositorio.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/grupotg/dominio/repositorio/GrupoTgRepositorio.java
@@ -1,13 +1,15 @@
 package br.edu.com.fateczl.sistema.gerenciador.tg.grupotg.dominio.repositorio;
 
-import br.edu.com.fateczl.sistema.gerenciador.tg.aluno.dominio.entidade.Aluno;
+import br.edu.com.fateczl.sistema.gerenciador.tg.aluno.dominio.objetosvalor.AlunoId;
+import br.edu.com.fateczl.sistema.gerenciador.tg.curso.dominio.objetosvalor.CursoId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.grupotg.dominio.entidade.GrupoTg;
-import br.edu.com.fateczl.sistema.gerenciador.tg.turma.dominio.entidade.Turma;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface GrupoTgRepositorio {
-    GrupoTg salvar(GrupoTg grupoTg);
-    Optional<GrupoTg> buscarPorAlunosETurma(List<Aluno> alunos, Turma turma);
+    void salvar(GrupoTg grupoTg);
+    Optional<GrupoTg> buscarPorAlunoIdECursoId(
+            AlunoId alunoId,
+            CursoId cursoId
+    );
 }
