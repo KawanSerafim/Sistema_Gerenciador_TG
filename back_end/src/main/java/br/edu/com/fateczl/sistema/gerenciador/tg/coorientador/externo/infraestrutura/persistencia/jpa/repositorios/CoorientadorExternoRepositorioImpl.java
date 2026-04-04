3f4ee5f4788321afbf8af8bd7650bd012555de8c
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-04-03 22:27:22 -0300

    refactor(coorientador externo): arrumado para utilizar o mapeador.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/coorientador/externo/infraestrutura/persistencia/jpa/repositorios/CoorientadorExternoRepositorioImpl.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/coorientador/externo/infraestrutura/persistencia/jpa/repositorios/CoorientadorExternoRepositorioImpl.java
@@ -2,15 +2,14 @@ package br.edu.com.fateczl.sistema.gerenciador.tg.coorientador.externo.infraestr
 
 import br.edu.com.fateczl.sistema.gerenciador.tg.compartilhado.dominio.objetosvalor.Nome;
 import br.edu.com.fateczl.sistema.gerenciador.tg.coorientador.externo.dominio.entidade.CoorientadorExterno;
-import br.edu.com.fateczl.sistema.gerenciador.tg.coorientador.externo.dominio.objetosvalor.CoorientadorExternoId;
 import br.edu.com.fateczl.sistema.gerenciador.tg.coorientador.externo.dominio.objetosvalor.Origem;
 import br.edu.com.fateczl.sistema.gerenciador.tg.coorientador.externo.dominio.repositorio.CoorientadorExternoRepositorio;
-import br.edu.com.fateczl.sistema.gerenciador.tg.coorientador.externo.infraestrutura.persistencia.jpa.modelo.CoorientadorExternoModelo;
+import br.edu.com.fateczl.sistema.gerenciador.tg.coorientador.externo.infraestrutura.persistencia.jpa.mapeador.CoorientadorExternoMapeador;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -19,29 +18,21 @@ public class CoorientadorExternoRepositorioImpl
     private final CoorientadorExternoJpaRepositorio repositorio;
 
     @Override
+    @Transactional
     public void salvar(CoorientadorExterno coorientadorExterno) {
-        var modelo = new CoorientadorExternoModelo(
-                coorientadorExterno.idTexto(),
-                coorientadorExterno.nomeTexto(),
-                coorientadorExterno.identificacao()
+        var modelo = CoorientadorExternoMapeador.paraModelo(
+                coorientadorExterno
         );
         repositorio.save(modelo);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<CoorientadorExterno> buscarPorNomeEOrigem(
             Nome nome,
             Origem origem
     ) {
         return repositorio.findByNomeAndOrigem(nome.valor(), origem.valor())
-                .map(this::paraDominio);
-    }
-
-    private CoorientadorExterno paraDominio(CoorientadorExternoModelo modelo) {
-        return CoorientadorExterno.carregar(
-                new CoorientadorExternoId(UUID.fromString(modelo.getId())),
-                new Nome(modelo.getNome()),
-                new Origem(modelo.getOrigem())
-        );
+                .map(CoorientadorExternoMapeador::paraDominio);
     }
 }
