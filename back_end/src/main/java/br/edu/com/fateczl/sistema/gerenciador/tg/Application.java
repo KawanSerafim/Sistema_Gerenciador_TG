91d8af05047ae2e1a7699f46bf22f5120e8f5077
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2026-04-11 22:02:34 -0300

    feat(principal): adicionado a anotação EnableAsync.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/Application.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tg/Application.java
@@ -2,8 +2,10 @@ package br.edu.com.fateczl.sistema.gerenciador.tg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
 	static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
